f87c1fd2e06cecf6444b72dcfbcca8ceabeaae8e
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2021-10-09 01:34:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let data = document.querySelector('.displyItoms');
+let searchData = document.querySelector("#searchData");
 let createData="";
-
+let newCreateData="";
 let product = [
     {
         name:"Realme x50 pro 5g",
@@ -13,11 +14,39 @@ let product = [
     {
         name:"OnePlus 9 Pro 5G",
         image:"OnePlus 9 Pro 5G.png",
-    }
+    },
+    {
+        name:"Realme 8i 5G",
+        image:"Realme 8i 5G.png",
+    },
+    {
+        name:"POCO M3 Pro",
+        image:"POCO M3 Pro all.png",
+    },
+    {
+        name:"Motorola G40 Fusion",
+        image:"Motorola G40 Fusion.png",
+    },
+    {
+        name:"ROG Phone 5 Ultimate",
+        image:"ROG Phone 5 Ultimate.png",
+    },
+    {
+        name:"iPhone 13 Pro Max",
+        image:"iPhone 13 Pro Max.png",
+    },
+    {
+        name:"ROG Phone 3",
+        image:"ROG Phone 3.png",
+    },
+    {
+        name:"ROG Phone 5s Pro",
+        image:"ROG Phone 5s Pro.png",
+    },
 ];
 
 for (let i = 0; i < product.length; i++) {
-createData = 
+const createData = 
 `<h3 class="d-flex justify-content-center">${product[i].name}</h3> 
 <div class="p-3 d-flex justify-content-center bg-image hover-zoom"> 
 <a href="https://fanse-online-shop.netlify.app/${product[i].image}"> <img src="${product[i].image}" alt="img"/></a> 
@@ -26,8 +55,36 @@ createData =
 <button class="btn btn-warning" type="button" >Add to Cart</button> 
 </div> `;
 let newCreateData=document.createElement('div');
-newCreateData.setAttribute("class","col-md-4")
+newCreateData.setAttribute("class","col-md-4");
 newCreateData.innerHTML=createData;
-console.log(createData);
 data.append(newCreateData);
-}
+};
+
+
+
+function search(){
+    data.innerHTML="";
+    cap = searchData.value.toUpperCase();
+    for (let i=0; i<product.length; i++){
+        text=product[i].name;
+        console.log(text.toUpperCase().indexOf(cap));
+       if(text.toUpperCase().indexOf(cap)>-1){
+        const createData = 
+        `<h3 class="d-flex justify-content-center">${product[i].name}</h3> 
+        <div class="p-3 d-flex justify-content-center bg-image hover-zoom"> 
+        <a href="https://fanse-online-shop.netlify.app/${product[i].image}"> <img src="${product[i].image}" alt="img"/></a> 
+        </div> 
+        <div class="p-3 d-flex justify-content-center"> 
+        <button class="btn btn-warning" type="button" >Add to Cart</button> 
+        </div> `;
+        let newCreateData=document.createElement('div');
+        newCreateData.setAttribute("class","col-md-4");
+        newCreateData.innerHTML=createData;
+        console.log(createData);
+        data.append(newCreateData);
+        }
+       else {
+        createData="none";
+       }
+       }
+    }
