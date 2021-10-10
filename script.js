af9e96f32ea9d59a74f1d5f10cc645082c21bf3a
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2021-10-10 16:54:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,99 +12,153 @@ let product = [
     {
         name:"Realme x50 pro 5G",
         image:"img/Realme X50 Pro 5g.png",
-        price: `₹41,999`,
-        rom:`128 GB`,
-        ram:`8 GB`,
+        price: `41,999`,
+        rom:`128`,
+        ram:`8`,
         processor:`Qualcomm Snapdragon 865`,
-        battry: `5000 mAh`,
+        battry: `5000`,
     },
     {
         name:"Redmi Note10 Pro Max",
         image:"img/Redmi Note 10 Pro Max.png",
-        price: `₹21,990`,
-        rom:`128 GB`,
-        ram:`6 GB`,
+        price: `21,990`,
+        rom:`128`,
+        ram:`6`,
         processor:`Qualcomm Snapdragon 732G`,
-        battry: `5000 mAh`,
+        battry: `5000`,
     },
     {
         name:"OnePlus 9 Pro 5G",
         image:"img/OnePlus 9 Pro 5G.png",
-        price: `₹60,999`,
-        rom:`128 GB`,
-        ram:`8 GB`,
+        price: `60,999`,
+        rom:`128`,
+        ram:`8`,
         processor:`Qualcomm Snapdragon 888`,
-        battry: `4500 mAh`,
+        battry: `4500`,
     },
     {
         name:"Realme 8i 5G",
         image:"img/Realme 8i 5G.png",
-        price: `₹14,999`,
-        rom:`128 GB`,
-        ram:`6 GB`,
+        price: `14,999`,
+        rom:`128`,
+        ram:`6`,
         processor:`MediaTek Helio G96`,
-        battry: `5000 mAh`,
+        battry: `5000`,
     },
     {
         name:"POCO M3 Pro",
         image:"img/POCO M3 Pro all.png",
-        price: `₹16,499`,
-        rom:`128 GB`,
-        ram:`6 GB`,
+        price: `16,499`,
+        rom:`128`,
+        ram:`6`,
         processor:`MediaTek Dimensity 700`,
-        battry: `5000 mAh`,
+        battry: `5000`,
     },
     {
         name:"Motorola G40 Fusion",
         image:"img/Motorola G40 Fusion.png",
-        price: `₹14,999`,
-        rom:`128 GB`,
-        ram:`6 GB`,
+        price: `14,999`,
+        rom:`128`,
+        ram:`6`,
         processor:`Qualcomm Snapdragon 732G`,
-        battry: `6000 mAh`,
+        battry: `6000`,
     },
     {
         name:"Asus ROG Phone 5 Ultimate",
         image:"img/ROG Phone 5 Ultimate.png",
-        price: `₹49,999`,
-        rom:`128 GB`,
-        ram:`8 GB`,
+        price: `49,999`,
+        rom:`128`,
+        ram:`8`,
         processor:`Qualcomm Snapdragon 888 (SM8350)`,
-        battry: `6000 mAh`,
+        battry: `6000`,
     },
     {
         name:"iPhone 13 Pro Max",
         image:"img/iPhone 13 Pro Max.png",
-        price: `₹1,39,900`,
-        rom:`256 GB`,
-        ram:`256 GB`,
+        price: `1,39,900`,
+        rom:`256`,
+        ram:`256`,
         processor:`A15 Bionic Chip`,
-        battry: `5000 mAh`,
+        battry: `5000`,
     },
     {
         name:"Asus ROG Phone 3",
         image:"img/ROG Phone 3.png",
-        price: `₹34,999`,
-        rom:`128 GB`,
-        ram:`8 GB`,
+        price: `34,999`,
+        rom:`128`,
+        ram:`8`,
         processor:`Qualcomm Snapdragon 865+ (SM8250)`,
-        battry: `6000 mAh`,
+        battry: `6000`,
+    },
+    {
+        name:"Realme GT Master Edition",
+        image:"img/realme GT Master Edition.png",
+        price: `22,999`,
+        rom:`128`,
+        ram:`8`,
+        processor:`Qualcomm Snapdragon 778G`,
+        battry: `4300`,
+    },
+    {
+        name:"Google Pixel 5 5G Sorta Sage",
+        image:"img/Google Pixel 5 5G.png",
+        price: `64,298`,
+        rom:`128`,
+        ram:`8`,
+        processor:`Qualcomm Snapdragon 765G`,
+        battry: `4080`,
+    },
+    {
+        name:"Meizu 18 Pro",
+        image:"img/Meizu 18 pro.png",
+        price: `65,999`,
+        rom:`256`,
+        ram:`8`,
+        processor:`Snapdragon 888 5G chipset`,
+        battry: `5000`,
+    },
+    {
+        name:"Oppo Reno 6 Pro 5G",
+        image:"img/oppo Reno6 pro 5G.png",
+        price: `42,990`,
+        rom:`256`,
+        ram:`12`,
+        processor:`MediaTek MT6893 Dimensity 1200`,
+        battry: `4500`,
     },
     {
         name:"Asus ROG Phone 5s Pro",
         image:"img/ROG Phone 5s Pro.png",
-        price: `₹41,999`,
-        rom:`512 GB`,
-        ram:`18 GB`,
+        price: `41,999`,
+        rom:`512`,
+        ram:`18`,
         processor:`Qualcomm Snapdragon 888+`,
-        battry: `6000 mAh`,
+        battry: `6000`,
+    },
+    {
+        name:"iQOO 7 5G",
+        image:"img/iQOO 7.jpg",
+        price: `29,990`,
+        rom:`128`,
+        ram:`8`,
+        processor:`Qualcomm Snapdragon 870`,
+        battry: `4400`,
+    },
+    {
+        name:"Nokia G10",
+        image:"img/Nokia G10.jpg",
+        price: `12,149`,
+        rom:`64`,
+        ram:`4`,
+        processor:`Mediatek G25 8x A53 2.0GHz`,
+        battry: `5050`,
     },
 ];
 
 var brands = [
     {
         brandName:"Realme",
-        icon:"icons/Realme.jpg",
+        icon:"icons/Realme.png",
     },
     {
         brandName:"Motorola",
@@ -115,8 +169,8 @@ var brands = [
         icon:"icons/Redmi.png",
     },
     {
-        brandName:"Asus",
-        icon:"icons/asus.jpg",
+        brandName:"ROG",
+        icon:"icons/ROG.png",
     },
     {
         brandName:"Poco",
@@ -136,7 +190,7 @@ var brands = [
     },
     {
         brandName:"OPPO",
-        icon:"icons/OPPO.jpg",
+        icon:"icons/OPPO.png",
     },
     {
         brandName:"meizu",
@@ -160,7 +214,11 @@ var brands = [
     },
     {
         brandName:"vivo",
-        icon:"icons/vivo.jpg",
+        icon:"icons/vivo.png",
+    },
+    {
+        brandName:"Asus",
+        icon:"icons/ASUS.png",
     },
 ];
 
@@ -172,12 +230,16 @@ const getImg=(()=>{
                                 <img class="p-3" id="img" src="${product[x].image}" alt="img"/>
                             </a> 
                         </div>
-                        <div class="col-md-6">
-                            <h1 class="p-2">${product[x].name}</h2>
-                            <p class="p-2"><span id="price"><b>Price:</b><span id="priceVal"> ${product[x].price}/-</span></span></p>
-                            <p class="p-2" id="rom"><span">RAM: ${product[x].ram}</span>  <span">   ROM: ${product[x].rom}</span></p>
-                            <p class="p-2" id="processor">Processor: ${product[x].processor}</p>
-                            <p class="p-2" id="battry">Battry: ${product[x].battry}</p>
+                        <div class="col-md-5" id="Details">
+                            <div class="p-1 fontSize1">${product[x].name}</div>
+                            <p class="p-1"><span id="price"><b>Price:</b><span id="priceVal"> ₹${product[x].price}.00</span></span></p>
+                            <p class="row p-1" id="rom"><span class="col-md-4 d-flex justify-content-start">RAM: ${product[x].ram} GB</span> <span class="col-md-8 d-flex justify-content-start">ROM: ${product[x].rom} GB</span></p>
+                            <p class="p-1" id="processor">Processor: ${product[x].processor}</p>
+                            <p class="p-1" id="battry">Battry: ${product[x].battry} mAh</p>
+                            <div class="d-grid gap-2 d-md-block">
+                                <button class="btn btn-warning m-2" type="button">Add to Cart</button>
+                                <button class="btn btn-dark btn-outline-warning m-2" type="button">Buy Now</button>
+                            </div>
                         </div>`;
 
     newCreateData=document.createElement('div');
@@ -189,7 +251,7 @@ const getImg=(()=>{
 
 // func to pront thhe product datails
 const getData=(()=>{
-    const createData = `<h4 class="d-flex justify-content-center">${product[x].name}</h4> 
+    const createData = `<div class="d-flex justify-content-center fontSize">${product[x].name}</div> 
                         <div class="p-3 d-flex justify-content-center bg-image hover-zoom"> 
                             <a href="https://fanse-online-shop.netlify.app/${product[x].image}">
                                 <img id="display" src="${product[x].image}" alt="img"/>
@@ -273,7 +335,7 @@ const search=(()=>{
             getData();
         }
         else{
-            createData="none";
+            error();
         }
     }
 });
@@ -283,4 +345,19 @@ const ViewDetails=((n)=>{
     data.innerHTML="";
     x=n;
     getImg();
+});
+
+
+const error=(()=>{
+    remData.innerHTML=""
+    data.innerHTML="";
+    const createData =
+                    `<div class="row d-flex justify-content-center">
+                        <img class="error mx-auto" src="not found img/pngegg (1).png" alt="">
+                    </div>
+                        <h3 class="row d-flex justify-content-center m-3">What is this..?</h3>`;
+    newCreateData=document.createElement('div');
+    newCreateData.setAttribute("class","row");
+    newCreateData.innerHTML=createData;
+    data.append(newCreateData);
 });
